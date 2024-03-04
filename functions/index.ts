4b5c2dcb61d36bd93b4158e3c2332dd8af0499ac
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2024-03-04 13:03:12 +0700

    fix: now caching in cloudflare kv

--- functions/index.ts
+++ functions/index.ts
@@ -1,17 +1,16 @@
-import { getJwks, useKVStore, verify, VerifyRsaJwtEnv, KVNamespaceOrKeyValueStore, GeneralKeyValueStore } from "verify-rsa-jwt-cloudflare-worker";
+import { getJwks, useKVStore, verify, KVNamespaceOrKeyValueStore } from "verify-rsa-jwt-cloudflare-worker";
 interface Env {
   JWKS_URI: string;
-  VERIFY_RSA_JWT: GeneralKeyValueStore;
+  JWKS_CACHE: KVNamespaceOrKeyValueStore;
 }
 
 export const onRequest: PagesFunction<Env> = async (context) => {
-  //   const value = await context.env.KV.get("example");
-  //   return new Response(value);
   const token = context.request.headers.get("Authorization")?.replace(/Bearer\s+/i, "") || "";
   try {
-    const jwks = await getJwks(context.env.JWKS_URI, useKVStore(context.env.VERIFY_RSA_JWT));
+    const jwks = await getJwks(context.env.JWKS_URI, useKVStore(context.env.JWKS_CACHE));
     const { payload } = await verify(token, jwks);
-    // Then, you could validate the payload and return a response
+    // TODO: validate the payload
+    // return a response
     return new Response(JSON.stringify({ payload }), {
       headers: { "content-type": "application/json" },
     });
