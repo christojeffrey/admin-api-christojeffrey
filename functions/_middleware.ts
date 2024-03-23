ed2c810a763616696b68d23ee0f481cf022218c0
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2024-03-23 22:22:41 +0700

    add cors

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -5,5 +5,11 @@ async function errorHandling(context) {
     return new Response(`${err.message}\n${err.stack}`, { status: 500 });
   }
 }
-
-export const onRequest = [errorHandling];
+// Set CORS to all /api responses
+export const corsHandling = async (context) => {
+  const response = await context.next();
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  response.headers.set("Access-Control-Max-Age", "86400");
+  return response;
+};
+export const onRequest = [errorHandling, corsHandling];
