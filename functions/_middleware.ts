8a1cca6d807c35b67aed47fee49d3428f935e252
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2024-03-26 12:28:41 +0700

    add cors on OPTION request

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -5,11 +5,24 @@ async function errorHandling(context) {
     return new Response(`${err.message}\n${err.stack}`, { status: 500 });
   }
 }
-// Set CORS to all /api responses
+
+export const onRequestOptions: PagesFunction = async () => {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "*",
+      "Access-Control-Allow-Methods": "GET, OPTIONS",
+      "Access-Control-Max-Age": "86400",
+    },
+  });
+};
+// Set CORS to all / responses
 export const corsHandling = async (context) => {
   const response = await context.next();
   response.headers.set("Access-Control-Allow-Origin", "*");
   response.headers.set("Access-Control-Max-Age", "86400");
   return response;
 };
-export const onRequest = [errorHandling, corsHandling];
+
+export const onRequest = [errorHandling, onRequestOptions, corsHandling];
